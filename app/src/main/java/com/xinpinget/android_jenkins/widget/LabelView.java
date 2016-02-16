48c8554a6d34d786ecc50bf5d34612cb7ae0455b
Frank Cheng <ufo22940268@gmail.com>
2016-02-16 19:08:24 +0800

    Add label view.

--- app/src/main/java/com/xinpinget/android_jenkins/widget/LabelView.java
+++ app/src/main/java/com/xinpinget/android_jenkins/widget/LabelView.java
@@ -4,16 +4,40 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import com.xinpinget.android_jenkins.R;
+
 /**
  * Created by cc on 2/16/16.
  */
 public class LabelView extends TextView {
+
+    enum State {
+        Normal(R.drawable.bg_normal_state, R.string.normal_state),
+        Error(R.drawable.bg_error_state, R.string.error_state);
+
+        private final int mBg;
+        private final int mState;
+
+        State(int bg, int state) {
+            mBg = bg;
+            mState = state;
+        }
+    }
 
     public LabelView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     public void setStatusColor(String color) {
-        setText(color + "iii");
+        State state = toState(color);
+        setBackgroundResource(state.mBg);
+        setText(state.mState);
+    }
+
+    private State toState(String color) {
+        if (color.equals("blue"))
+            return State.Normal;
+        else
+            return State.Error;
     }
 }
